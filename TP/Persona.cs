e6f1e1d2ed38ef859ece61275aee43c7c032157e
lisandroferroni <lisandro.ferroni@gmail.com>
2018-06-07 00:52:00 -0300

    persona test

---
+++ TP/Persona.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP
+{
+    public class Persona
+    {
+        private string _Nombre, _Apellido;
+        private DateTime _FechaNac;
+        public string Nombre
+        {
+            set
+            {
+                _Nombre = value;
+            }
+            get
+            {
+                return _Nombre;
+            }
+        }
+        public string Apellido
+        {
+            set
+            {
+                _Apellido = value;
+            }
+            get
+            {
+                return _Apellido;
+            }
+        }
+        public DateTime FechaNac
+        {
+            set
+            {
+                _FechaNac = value;
+            }
+            get
+            {
+                return _FechaNac;
+            }
+        }
+        public Persona (string nombre, string apellido, DateTime fechaNac)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            FechaNac = fechaNac;
+        }
+        public int GetEdad()
+        {
+            int edad = DateTime.Today.AddTicks(-FechaNac.Ticks).Year - 1;
+
+            ///double dias = (DateTime.Today - FechaNac).TotalDays;
+            Console.WriteLine(edad);
+            return edad;
+        }
+
+
+
+    }
+
+    
+}
---
+++ TPTests3/PersonaTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP.Tests
+{
+    [TestClass()]
+    public class PersonaTests
+    {
+        [TestMethod()]
+        public void PersonaTest()
+        {
+            ///Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetEdadTest()
+        {
+            DateTime fecha = new DateTime(2000, 6, 8);
+            Persona seba = new Persona("seba","jaja",fecha);
+            var actual = seba.GetEdad();
+            const int esperado = 18;
+            Assert.AreEqual(actual,esperado);
+        }
+    }
+}
